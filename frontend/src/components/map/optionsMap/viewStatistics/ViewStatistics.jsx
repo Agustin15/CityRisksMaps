8de434309e4855426d9016ff1c0f4b7ae27cb4e6
Agustin15 <agus20m05@gmail.com>
2026-01-26 18:40:09 -0300

    feat:option access to formQuiz through infoWindowNeighborhood and loadingMenu added

--- frontend/src/components/map/optionsMap/viewStatistics/ViewStatistics.jsx
+++ frontend/src/components/map/optionsMap/viewStatistics/ViewStatistics.jsx
@@ -1,9 +1,9 @@
 import styles from "./ViewStatistics.module.css";
+import { useState, useEffect, useId, Activity } from "react";
 import { useMapControls } from "../../../../contexts/MapContext";
 import { useZoneCrimes } from "../../../../contexts/zoneCrimesContext/ZoneCrimesContext";
 import { useQuizes } from "../../../../contexts/quizesContext/QuizesContext.jsx";
 import { useWindowResize } from "../../../../contexts/WindowResizeContext.jsx";
-import { useState, useEffect, useId, Activity } from "react";
 import { Menu } from "./menu/Menu.jsx";
 import { ContainQuizes } from "../../quizes/containQuizes/ContainQuizes.jsx";
 import { CrimeNeighbordhoods } from "../../crimeNeighData/CrimeNeighbordhoods.jsx";
