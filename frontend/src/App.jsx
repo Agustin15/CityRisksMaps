467c3f3a887210ad8ae1e9c59d4af1ef0cdfae02
Agustin15 <agus20m05@gmail.com>
2026-01-31 19:57:59 -0300

    feat:notFoundPage component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
-import { APIProvider } from "@vis.gl/react-google-maps";
-import { ContainMap } from "./components/map/ContainMap";
-import { MapProvider } from "./contexts/MapContext";
-import { RoutesProvider } from "./contexts/routesContext/RoutesContext";
-import { PhotosProvider } from "./contexts/PhotosContext";
-import { ZoneCrimesProvider } from "./contexts/zoneCrimesContext/ZoneCrimesContext";
-import { QuizesProvider } from "./contexts/quizesContext/QuizesContext";
-import { SearchPlaceProvider } from "./contexts/SearchPlaceContext";
-import { NavigationProvider } from "./contexts/NavigationContext";
-const API_KEY = import.meta.env.VITE_MAPS_API_KEY;
+import { MapPage } from "./components/Pages/MapPage.jsx";
+import { CookiesProvider } from "react-cookie";
+import { WindowResizeProvider } from "./contexts/WindowResizeContext.jsx";
+import { NotFoundPage } from "./components/pages/notFoundPage.jsx";
 
 function App() {
   return (
-    <APIProvider apiKey={API_KEY}>
-      <MapProvider>
-        <PhotosProvider>
-          <ZoneCrimesProvider>
-            <SearchPlaceProvider>
-              <QuizesProvider>
-                <RoutesProvider>
-                  <NavigationProvider>
-                    <ContainMap />
-                  </NavigationProvider>
-                </RoutesProvider>
-              </QuizesProvider>
-            </SearchPlaceProvider>
-          </ZoneCrimesProvider>
-        </PhotosProvider>
-      </MapProvider>
-    </APIProvider>
+    <CookiesProvider>
+      <WindowResizeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MapPage />}></Route>
+            <Route path="/*" element={<NotFoundPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </WindowResizeProvider>
+    </CookiesProvider>
   );
 }
 
---
+++ frontend/src/components/pages/MapPage.jsx
@@ -0,0 +1,32 @@
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { ContainMap } from "../../components/map/ContainMap";
+import { MapProvider } from "../../contexts/MapContext";
+import { RoutesProvider } from "../../contexts/routesContext/RoutesContext";
+import { PhotosProvider } from "../../contexts/PhotosContext";
+import { ZoneCrimesProvider } from "../../contexts/zoneCrimesContext/ZoneCrimesContext";
+import { QuizesProvider } from "../../contexts/quizesContext/QuizesContext";
+import { SearchPlaceProvider } from "../../contexts/SearchPlaceContext";
+import { NavigationProvider } from "../../contexts/NavigationContext";
+const API_KEY = import.meta.env.VITE_MAPS_API_KEY;
+
+export const MapPage = () => {
+  return (
+    <APIProvider apiKey={API_KEY}>
+      <MapProvider>
+        <PhotosProvider>
+          <ZoneCrimesProvider>
+            <SearchPlaceProvider>
+              <QuizesProvider>
+                <RoutesProvider>
+                  <NavigationProvider>
+                    <ContainMap />
+                  </NavigationProvider>
+                </RoutesProvider>
+              </QuizesProvider>
+            </SearchPlaceProvider>
+          </ZoneCrimesProvider>
+        </PhotosProvider>
+      </MapProvider>
+    </APIProvider>
+  );
+};
