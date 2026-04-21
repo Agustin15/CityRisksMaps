5d01d8a33f5a76b3c5bcc38d2d2ce00370bc315b
Agustin15 <agus20m05@gmail.com>
2026-04-21 18:07:09 -0300

    fix:captures of README.md changed

--- backend/src/route/routeLogout.js
+++ backend/src/route/routeLogout.js
@@ -6,6 +6,8 @@ RoutesLogout.post("/", (req, res) => {
   try {
     res.clearCookie("authenticationToken");
     res.clearCookie("authenticationRefreshToken");
+
+    res.status(200).json(true);
   } catch (error) {
     res.status(502).json({ messageError: error.message });
   }
