7dfb35521a17b1b9cb37eda968b1a3763df499b1
Agustin15 <agus20m05@gmail.com>
2026-08-13 17:35:51 -0300

    design of viewStatitcs in desktop updated

--- frontend/src/components/map/notData/NotData.jsx
+++ frontend/src/components/map/notData/NotData.jsx
@@ -1,13 +1,16 @@
+import iconNotData from "../../../assets/img/notData.png";
 import iconNotDataMobile from "../../../assets/img/notDataAlert.png";
 import { useNeighborhoodsCrimes } from "../../../contexts/neighborhoodsCrimesContext/NeighborhoodsCrimesContextContext";
+import { useWindowResize } from "../../../contexts/WindowResizeContext";
 import styles from "./NotData.module.css";
 
 export const NotData = () => {
   const { errorLoad } = useNeighborhoodsCrimes();
+  const { windowWidth } = useWindowResize();
 
   return (
     <div className={styles.notData}>
-      <img src={iconNotDataMobile}></img>
+      <img src={windowWidth >= 1200 ? iconNotData : iconNotDataMobile}></img>
       <span>{errorLoad}</span>
     </div>
   );
